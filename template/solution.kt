e7e94c4c330b0eee84e8dd1db761e8281a7c553c
Andreea C. Micu <andreea.micu.web@gmail.com>
2023-02-08 14:12:31 +0200

    init project template

---
+++ template/solution.kt
@@ -0,0 +1,47 @@
+import java.io.File
+
+val sample = """
+
+""".trimMargin()
+
+
+fun main() {
+	testSamplePart1()
+	testSamplePart2()
+}
+
+private fun solvePart1(): Int {
+	return 0
+}
+
+private fun solvePart2(): Int {
+	return 0
+}
+
+private fun readInput(fileName: String): List<Int> {
+	return File(fileName).readLines().map(String::toInt)
+}
+
+private fun readSample(): List<Int> {
+	return sample.split("\n").map(String::toInt)
+}
+
+private fun testSamplePart1() {
+	val resultExpected = 1
+	val resultActual = solvePart1()
+	if(resultExpected != resultActual) {
+		println("Part1 sample fails! Got $resultActual, expected $resultExpected")
+	} else {
+		println("Part1 sample passes") 
+	}
+}
+
+private fun testSamplePart2() {
+	val resultExpected = 1
+	val resultActual = solvePart2()
+	if(resultExpected != resultActual) {
+		println("Part2 sample fails! Got $resultActual, expected $resultExpected")
+	} else {
+		println("Part2 sample passes") 
+	}
+}
